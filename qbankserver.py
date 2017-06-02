618ee77acc6e136643c3d964cffedd6c2066b232
Prasanna Lakkur Subramanyam <prasanna.pessu@gmail.com>
2017-06-02 13:44:49 +0530

    server implemented

--- qbankserver.py
+++ qbankserver.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from flask import request
+import qbank
+
+QUESTION = 'question'
+ACTION = 'action'
+SUGGESTION = 'suggestion'
+NEW = 'new'
 
 host = '0.0.0.0'
 portNumber = 12223
@@ -9,17 +16,26 @@ def keyGet(key):
     return _serverAllQuestions(key)
 
 @app.route('/<key>', methods=['POST'])
-def keyGet(key):
-    return _serverAllQuestions(key)
+def keyPost(key):
+    question = request.form.get(QUESTION)
+    action = request.form.get(ACTION)
+    if action == SUGGESTION:
+        qli = qbank.getSuggestions(key, question);
+        return json.dumps(qli)
+    elif action == NEW:
+        qbank.append(key, question)
+        return _serverAllQuestions(key)
 
 
 @app.route('/<key>', methods=['DELETE'])
-def keyGet(key):
+def keyDel(key):
+    question = request.form.get(QUESTION)
+    qbank.delete(key, question)
     return _serverAllQuestions(key)
 
 
 def _serverAllQuestions(key):
-    return json.dumps(getQuestions)
+    return json.dumps(qbank.getQuestions(key))
 
 if __name__ == '__main__':
     app.run(host=host, port=portNumber, threaded=True)
